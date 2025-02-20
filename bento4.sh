810e45b5937a0d1a36534cbf59b0b462cad21c83
Unknown-903 <shubhamsinghal05072008@gmail.com>
2025-02-20 20:33:09 +0530

    Initial commit

---
+++ bento4.sh
@@ -0,0 +1,1 @@
+apt install -y git cmake make build-essential; git clone https://github.com/axiomatic-systems/Bento4.git; cd Bento4; mkdir cmakebuild; cd cmakebuild/; cmake -DCMAKE_BUILD_TYPE=Release ..; make;  
